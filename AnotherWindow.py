db183cb81cce6ab8f663e09e85d47c8c4192c435
Gustavo Roque <gustavo.roque.143@gmail.com>
2023-09-05 15:05:15 -0600

    Query the table

--- AnotherWindow.py
+++ AnotherWindow.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel, QSqlQuery, QSqlQueryModel
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QTableView,QVBoxLayout
 # from QWid
 # Initialize the Qt application
@@ -25,18 +25,20 @@ class DBTabel(QWidget):
         db.setDatabaseName("postgres")
         db.setUserName("postgres")
         db.setPassword("Admin4")
-        db.open()
-        # if db.open():
-        #     print('Todo Ok')
-        # else:
-        #     print('Error')
-        # # self.StartCom()
+        # db.open()
         self.table = QTableView()
-        self.model = QSqlTableModel(db=db)
+        self.model = QSqlQueryModel()
         self.table.setModel(self.model)
-        self.model.setTable("public.plu")
-        self.model.select()
-        self.setMinimumSize(QSize(1024, 600))
+        if db.open():
+            # print('Todo Ok')
+            quer = "SELECT * FROM public.plu;"
+            q = QSqlQuery(quer,db=db)
+            self.model.setQuery(q)
+            # self.model.select()
+            # self.setMinimumSize(QSize(1024, 600))
+        else:
+            print('Error')
+        # # self.StartCom()
         # self.setCentralWidget(self.table)
         lay.addWidget(self.table)
         self.setLayout(lay)
